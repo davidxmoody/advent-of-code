a65c0dc2f8edb7de9617b6b9b545ef764061e3d1
David Moody <david@davidxmoody.com>
2019-03-05 19:09:22 +0000

    Add day 10 first star

---
+++ day-10/1-first-star.sql
@@ -0,0 +1,25 @@
+-- Puzzle input: 7347
+
+CREATE OR REPLACE FUNCTION d10_power_level(x int, y int, serial_number int) RETURNS int LANGUAGE sql AS $$
+  SELECT ((((((x + 10) * y) + serial_number) * (x + 10)) / 100) % 10) - 5;
+$$;
+
+CREATE VIEW d10_grid AS
+  SELECT *
+  FROM generate_series(1, 300) AS x
+  FULL OUTER JOIN generate_series(1, 300) AS y
+  ON TRUE;
+
+SELECT
+  d10_power_level(x+0, y+0, 7347) +
+  d10_power_level(x+0, y+1, 7347) +
+  d10_power_level(x+0, y+2, 7347) +
+  d10_power_level(x+1, y+0, 7347) +
+  d10_power_level(x+1, y+1, 7347) +
+  d10_power_level(x+1, y+2, 7347) +
+  d10_power_level(x+2, y+0, 7347) +
+  d10_power_level(x+2, y+1, 7347) +
+  d10_power_level(x+2, y+2, 7347) AS total_power,
+  x, y
+FROM d10_grid
+ORDER BY 1 DESC LIMIT 1;
