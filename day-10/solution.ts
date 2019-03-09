fa4dcb4159d0400f8d9043771e3fde5ad5bd0c44
David Moody <david@davidxmoody.com>
2019-03-09 11:18:42 +0000

    Add much faster TypeScript solution to day 10
    Down from 2 mins 20 seconds to just 5 seconds.

--- day-10/solution.ts
+++ day-10/solution.ts
@@ -1,9 +1,6 @@
 import data from "./data"
 
 function sum(xStart: number, yStart: number, size: number): number | null {
-  if (xStart + size - 1 >= 300) return null
-  if (yStart + size - 1 >= 300) return null
-
   let total = 0
 
   for (let x = xStart; x < xStart + size; x++) {
@@ -16,23 +13,48 @@ function sum(xStart: number, yStart: number, size: number): number | null {
 }
 
 let largest = -Infinity
+let largestX
+let largestY
+let largestSize
 
 for (let size = 1; size <= 300; size++) {
   const greatestPossible = size * size * 4
-  foo:
-  for (let x = 0; x < 300; x++) {
-    for (let y = 0; y < 300; y++) {
-      const num = sum(x, y, size)
-      if (num != null && num > largest) {
-        console.log(`Old: ${largest},	new: ${num},	x: ${x+1},	y: ${y+1},	size: ${size}`)
-        largest = num
+
+  nextSize:
+  for (let y = 0; y <= (300 - size); y++) {
+    const columnSums = []
+    for (let i = 0; i < 300; i++) {
+      let columnSum = 0
+      for (let j = 0; j < size; j++) {
+        columnSum += data[y + j][i]
       }
+      columnSums.push(columnSum)
+    }
+
+    for (let x = 0; x <= (300 - size); x++) {
+      let num = 0
+      for (let i = 0; i < size; i++) {
+        num += columnSums[x + i]
+      }
+
+      if (num != null && num > largest) {
+        console.log(
+          `Sum: ${num},	x: ${x + 1},	y: ${y + 1},	size: ${size}`,
+        )
+        largest = num
+        largestX = x
+        largestY = y
+        largestSize = size
+      }
+
       if (num === greatestPossible) {
-        console.log(`Reached greatest possible: x=${x+1}, y=${y+1}, size=${size+1}`)
-        continue foo
+        // Reached greatest possible for this size
+        continue nextSize
       }
     }
   }
 }
+
+console.log(`${largestX},${largestY},${largestSize}`)
 
 // Solution: num=91, x=233, y=228, size=12
