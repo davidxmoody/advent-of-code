eb318b0da64614e49f02bc9f2a69d693234909f8
David Moody <david@davidxmoody.com>
2019-12-21 12:37:57 +0000

    Refactor star calculation

--- src/day01/star1.ts
+++
@@ -1,6 +0,0 @@
-import data from "./data.json"
-import {getFuel} from "./getFuel"
-
-const totalFuelRequirement = data.map(getFuel).reduce((a, b) => a + b)
-
-console.log(totalFuelRequirement)
--- src/day01/tests.ts
+++ src/day01/tests.ts
@@ -1,16 +1,22 @@
 import {getFuel} from "./getFuel"
 import assert from "assert"
+import data from "./data.json"
 
-const testCases = [
-  [0, 0],
-  [12, 2],
-  [14, 2],
-  [1969, 654],
-  [100756, 33583],
-]
+describe("getFuel", () => {
+  const testCases = [[0, 0], [12, 2], [14, 2], [1969, 654], [100756, 33583]]
 
-testCases.forEach(([input, output]) => {
-  it(`getFuel(${input}) === ${output}`, () => {
-    assert(getFuel(input) === output)
+  testCases.forEach(([input, output]) => {
+    it(`getFuel(${input}) === ${output}`, () => {
+      assert(getFuel(input) === output)
+    })
+  })
+})
+
+describe("Star 1", () => {
+  const correctAnswer = 3363929
+  const totalFuelRequirement = data.map(getFuel).reduce((a, b) => a + b)
+
+  it(`returns ${correctAnswer}`, () => {
+    assert(totalFuelRequirement === correctAnswer)
   })
 })
