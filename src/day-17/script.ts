dee25a7783e80231c3465b5aeccac99f0888f246
David Moody <david@davidxmoody.com>
2023-01-25 11:11:13 +0000

    Add day 17 star 2

--- src/day-17/script.ts
+++ src/day-17/script.ts
@@ -1,149 +1,167 @@
 import readLines from "../helpers/readLines"
 
-type Direction = "<" | ">" | "down"
-type Vector = [number, number] // y, x
-type Position = [number, number] // y, x
-type Shape = Vector[]
-type World = boolean[][] // y, x
+// This problem went through three different attempts:
+//
+// 1. Represented world/shapes as arrays of boolean arrays (worked for star 1).
+//
+// 2. Expected star 2 to be unachievable with pure simulation but wanted to try
+//    to see how fast I could make the code anyway. The initial boolean array
+//    attempt ran out of memory quickly. I updated it to use a Buffer to
+//    represent the world where each row was a byte (first bit ignored). The
+//    Buffer cycled back to the start to avoid running out of memory. From
+//    running simulations with smaller numbers, I estimate that it would have
+//    taken approx 48 hours to do the full 1000000000000 iterations.
+//
+// 3. Noticed that every time the input did a full cycle, a consistent number
+//    of shapes were added which resulted in a consistent height gain (except
+//    for the very first cycle which was slightly different). I wrote some
+//    hacky code to detect this cycle length and stop early once enough data
+//    had been collected.
+
+type Direction = "<" | ">"
+
+type World = Buffer // each byte is a row (first bit of each byte is ignored)
+
+interface Shape {
+  rows: number[] // byte array, starting from bottom (first bit ignored)
+  width: number
+}
 
 const input = readLines(__dirname + "/input.txt")[0].split("") as Direction[]
 
-const directionVectors: Record<Direction, Vector> = {
-  "<": [0, -1],
-  ">": [0, 1],
-  down: [-1, 0],
-}
+const worldMaxSize = 1000
 
 const shapes: Shape[] = [
-  // ####
-  [
-    [0, 0],
-    [0, 1],
-    [0, 2],
-    [0, 3],
-  ],
+  // ####...
+  {
+    rows: [0b1111000],
+    width: 4,
+  },
 
-  // .#.
-  // ###
-  // .#.
-  [
-    [0, 1],
-    [1, 0],
-    [1, 1],
-    [1, 2],
-    [2, 1],
-  ],
+  // .#.....
+  // ###....
+  // .#.....
+  {
+    rows: [0b0100000, 0b1110000, 0b0100000],
+    width: 3,
+  },
 
-  // ..#
-  // ..#
-  // ###
-  [
-    [0, 0],
-    [0, 1],
-    [0, 2],
-    [1, 2],
-    [2, 2],
-  ],
+  // ..#....
+  // ..#....
+  // ###....
+  {
+    rows: [0b1110000, 0b0010000, 0b0010000],
+    width: 3,
+  },
 
-  // #
-  // #
-  // #
-  // #
-  [
-    [0, 0],
-    [1, 0],
-    [2, 0],
-    [3, 0],
-  ],
+  // #......
+  // #......
+  // #......
+  // #......
+  {
+    rows: [0b1000000, 0b1000000, 0b1000000, 0b1000000],
+    width: 1,
+  },
 
-  // ##
-  // ##
-  [
-    [0, 0],
-    [0, 1],
-    [1, 0],
-    [1, 1],
-  ],
+  // ##.....
+  // ##.....
+  {
+    rows: [0b1100000, 0b1100000],
+    width: 2,
+  },
 ]
 
-function isEmpty(position: Position, world: World): boolean {
-  return !world[position[0]]?.[position[1]]
-}
-
-function move(position: Position, vector: Vector): Position {
-  return [position[0] + vector[0], position[1] + vector[1]]
-}
-
-function canShapeMove(
+function doesShapeFit(
+  y: number,
+  x: number,
   shape: Shape,
-  position: Position,
-  direction: Direction,
   world: World,
 ): boolean {
-  const nextPosition = move(position, directionVectors[direction])
-  const shapePiecePositions = shape.map((v) => move(nextPosition, v))
-  return shapePiecePositions.every(
-    (p) => p[0] >= 0 && p[1] >= 0 && p[1] < 7 && isEmpty(p, world),
+  if (y < 0 || x < 0 || x + shape.width > 7) return false
+
+  return shape.rows.every(
+    (shapeRow, rowIndex) =>
+      !((shapeRow >> x) & world[(y + rowIndex) % worldMaxSize]),
   )
 }
 
-function commitShapeToWorld(shape: Shape, position: Position, world: World) {
-  const shapePiecePositions = shape.map((v) => move(position, v))
-  const maxY = Math.max(
-    world.length - 1,
-    ...shapePiecePositions.map((p) => p[0]),
-  )
-
-  while (world.length - 1 < maxY) {
-    world.push([false, false, false, false, false, false, false])
-  }
-
-  for (const [y, x] of shapePiecePositions) {
-    world[y][x] = true
-  }
+function commitShapeToWorld(y: number, x: number, shape: Shape, world: World) {
+  shape.rows.forEach((shapeRow, rowIndex) => {
+    const bufferIndex = (y + rowIndex) % worldMaxSize
+    world[bufferIndex] = world[bufferIndex] | (shapeRow >> x)
+  })
 }
 
-function printWorld(world: World) {
-  for (const row of [...world].reverse()) {
-    console.log(row.map((b) => (b ? "#" : ".")).join(""))
-  }
-}
+// function printWorld(world: World) {
+//   for (let i = 40; i >= 0; i--) {
+//     console.log(
+//       world[i]
+//         .toString(2)
+//         .padStart(7, "0")
+//         .replaceAll("0", ".")
+//         .replaceAll("1", "#"),
+//     )
+//   }
+// }
 
-function simulate(numShapes: number): World {
-  const world: World = []
+function simulate(numShapes: number) {
+  const world: World = Buffer.alloc(worldMaxSize)
   let inputIndex = 0
 
+  let numFilledRows = 0
+  let cycle1: {numShapes: number; numFilledRows: number} | null = null
+  let cycle2: {numShapes: number; numFilledRows: number} | null = null
+
   for (let shapeIndex = 0; shapeIndex < numShapes; shapeIndex++) {
-    if (shapeIndex % 100 === 0) console.log(shapeIndex)
-    const shape = shapes[shapeIndex % shapes.length]
-    let shapePosition: Position = [world.length + 3, 2]
+    let shape = shapes[shapeIndex % shapes.length]
+    let shapeY = numFilledRows + 3
+    let shapeX = 2
+    for (let h = numFilledRows; h <= shapeY + 3; h++) {
+      world[h % worldMaxSize] = 0b0000000
+    }
 
     while (true) {
-      const blowDirection = input[inputIndex % input.length]
-      const canBeBlown = canShapeMove(
-        shape,
-        shapePosition,
-        blowDirection,
-        world,
-      )
-      if (canBeBlown) {
-        shapePosition = move(shapePosition, directionVectors[blowDirection])
+      const maybeNextShapeX =
+        input[inputIndex % input.length] === "<" ? shapeX - 1 : shapeX + 1
+      if (doesShapeFit(shapeY, maybeNextShapeX, shape, world)) {
+        shapeX = maybeNextShapeX
       }
       inputIndex++
 
-      const canFallDown = canShapeMove(shape, shapePosition, "down", world)
-      if (canFallDown) {
-        shapePosition = move(shapePosition, directionVectors.down)
+      if (inputIndex % input.length === 0) {
+        if (!cycle1) {
+          cycle1 = {numShapes: shapeIndex, numFilledRows: numFilledRows}
+        } else if (!cycle2) {
+          cycle2 = {
+            numShapes: shapeIndex - cycle1.numShapes,
+            numFilledRows: numFilledRows - cycle1.numFilledRows,
+          }
+        }
+      }
+
+      if (cycle2 && (numShapes - shapeIndex) % cycle2.numShapes === 0) {
+        const numRemainingCycles = (numShapes - shapeIndex) / cycle2.numShapes
+        return {
+          world,
+          numFilledRows:
+            numFilledRows + numRemainingCycles * cycle2.numFilledRows,
+        }
+      }
+
+      if (doesShapeFit(shapeY - 1, shapeX, shape, world)) {
+        shapeY--
       } else {
         break
       }
     }
 
-    commitShapeToWorld(shape, shapePosition, world)
+    commitShapeToWorld(shapeY, shapeX, shape, world)
+    numFilledRows = Math.max(numFilledRows, shapeY + shape.rows.length)
   }
 
-  return world
+  return {world, numFilledRows}
 }
 
-console.log("Star 1:", simulate(2022).length)
-// console.log("Star 1:", simulate(1000000000000).length)
+console.log("Star 1:", simulate(2022).numFilledRows)
+
+console.log("Star 2:", simulate(1000000000000).numFilledRows)
