b5ecd525759418e5abab02abfeba7f06ad556c94
David Moody <david@davidxmoody.com>
2023-12-12 13:29:07 +0000

    Add 2023 day 3 star 2

--- 2023-python/day-03/script.py
+++ 2023-python/day-03/script.py
@@ -5,31 +5,54 @@ with open("day-03/input.txt", "r") as f:
     data = f.readlines()
 
 
-def has_surrounding_symbol(y: int, x: int):
+def is_symbol(char: str):
+    return re.fullmatch(r"[^0-9.\n]", char)
+
+
+def is_digit(char: str):
+    return re.fullmatch(r"[0-9]", char)
+
+
+def surrounding_points(y: int, x: int):
     for y2 in range(max(0, y - 1), min(len(data) - 1, y + 1) + 1):
         for x2 in range(max(0, x - 1), min(len(data[0]) - 1, x + 1) + 1):
-            char = data[y2][x2]
-            if re.match(r"[^0-9.\n]", char):
-                return True
-    return False
+            yield (y2, x2)
 
 
-total = 0
+def capture_number(y: int, x: int):
+    start = x
+    while start > 0 and is_digit(data[y][start - 1]):
+        start -= 1
 
-current_num = 0
-current_symbol = False
+    end = x
+    while end < (len(data[y]) - 1) and is_digit(data[y][end + 1]):
+        end += 1
+
+    return int(data[y][start : end + 1])
+
+
+def find_surrounding_numbers(y: int, x: int):
+    surrounding: set[int] = set()
+    for y2, x2 in surrounding_points(y, x):
+        if is_digit(data[y2][x2]):
+            surrounding.add(capture_number(y2, x2))
+    return list(surrounding)
+
+
+part_number_total = 0
+gear_ratio_total = 0
 
 for y, line in enumerate(data):
     for x, char in enumerate(line):
-        try:
-            digit = int(char)
-            current_num = current_num * 10 + digit
-            current_symbol = current_symbol or has_surrounding_symbol(y=y, x=x)
+        if is_symbol(char):
+            numbers = find_surrounding_numbers(y, x)
 
-        except ValueError:
-            if current_symbol:
-                total += current_num
-            current_num = 0
-            current_symbol = False
+            for n in numbers:
+                part_number_total += n
 
-print(f"Star 1: {total}")
+            if char == "*" and len(numbers) == 2:
+                gear_ratio_total += numbers[0] * numbers[1]
+
+print(f"Star 1: {part_number_total}")
+
+print(f"Star 2: {gear_ratio_total}")
