4d3169469b94e97d4c45211241d0c9152fa9053b
David Moody <david@davidxmoody.com>
2023-12-21 16:30:51 +0000

    Add 2023 day 10

---
+++ 2023-python/day-10/script.py
@@ -0,0 +1,69 @@
+from numpy import array
+
+data = array([list(line[:-1]) for line in list(open("day-10/input.txt"))])
+
+north = (-1, 0)
+south = (1, 0)
+west = (0, -1)
+east = (0, 1)
+
+pipes = {
+    "|": [north, south],
+    "-": [east, west],
+    "L": [north, east],
+    "J": [north, west],
+    "7": [south, west],
+    "F": [south, east],
+    ".": [],
+}
+
+
+def reverse(s: tuple[int, int]):
+    return (s[0] * -1, s[1] * -1)
+
+
+def walk(p: tuple[int, int], s: tuple[int, int]):
+    return (p[0] + s[0], p[1] + s[1])
+
+
+def is_connected(p: tuple[int, int], s: tuple[int, int]):
+    p2 = walk(p, s)
+    backsteps = pipes[data[p2]]
+    return any([s2 == reverse(s) for s2 in backsteps])
+
+
+start = (0, 0)
+for y, line in enumerate(data):
+    for x, char in enumerate(line):
+        if char == "S":
+            start = (y, x)
+            for char, steps in pipes.items():
+                if all([is_connected(start, step) for step in steps]):
+                    data[start] = char
+                    break
+
+main_pipe = array([["." for _ in line] for line in data])
+
+current = start
+direction = pipes[data[start]][0]
+distance = 0
+
+while distance == 0 or current != start:
+    main_pipe[current] = data[current]
+    current = walk(current, direction)
+    direction = [s for s in pipes[data[current]] if reverse(s) != direction][0]
+    distance += 1
+
+print("Star 1:", distance // 2)
+
+num_inside = 0
+for line in main_pipe:
+    for x, char in enumerate(line):
+        if char == ".":
+            num_crossings = sum(
+                [1 for char2 in line[x + 1 :] if char2 in ("|", "J", "L")]
+            )
+            if num_crossings % 2 == 1:
+                num_inside += 1
+
+print("Star 2:", num_inside)
