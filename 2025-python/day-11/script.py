8076b4651aa049ad31eb6f04726100a914690912
David Moody <david@davidxmoody.com>
2025-12-12 13:39:32 +0000

    Add 2025 day 11 star 2

--- 2025-python/day-11/script.py
+++ 2025-python/day-11/script.py
@@ -1,10 +1,48 @@
-data = {line[:3]: line[5:-1].split() for line in open("day-11/input.txt")}
+data = {line[:3]: set(line[5:-1].split()) for line in open("day-11/input.txt")}
+
+inverted: dict[str, set[str]] = {}
+for src, dests in data.items():
+    for dest in dests:
+        if dest not in inverted:
+            inverted[dest] = set()
+        inverted[dest].add(src)
+
+
+def find_reachable(start: str):
+    reachable = set[str]()
+    unexplored = {start}
+
+    while unexplored:
+        next = unexplored.pop()
+        reachable.add(next)
+        if next in data:
+            for child in data[next]:
+                if child not in reachable:
+                    unexplored.add(child)
+
+    return reachable
 
 
 def count_paths(start: str, end: str):
-    if start == end:
-        return 1
-    return sum(count_paths(node, end) for node in data[start])
+    reachable = find_reachable(start)
+    unknown = set(reachable) - {start}
+    known = {start: 1}
+
+    while end not in known:
+        for dest in unknown:
+            if all((src in known or src not in reachable) for src in inverted[dest]):
+                known[dest] = sum(known.get(src, 0) for src in inverted[dest])
+                unknown.remove(dest)
+                break
+        else:
+            raise Exception("Cannot find path")
+
+    return known[end]
 
 
 print(f"Star 1: {count_paths("you", "out")}")
+
+
+print(
+    f"Star 2: {count_paths("svr", "fft") * count_paths("fft", "dac") *count_paths("dac", "out")}"
+)
